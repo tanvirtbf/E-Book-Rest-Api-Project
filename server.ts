7d2b36760f7582b5e1c1d19f530160114ed31005
tanvirtbf <tanver018765@gmail.com>
2024-10-15 23:17:59 +0600

    Database connected

--- server.ts
+++ server.ts
@@ -1,7 +1,13 @@
 import app from "./src/app";
+import { config } from "./src/config/config";
+import connectDb from "./src/config/db";
 
-const startServer = ()=>{
-    const port = process.env.PORT || 3000;
+const startServer = async()=>{
+
+    await connectDb()
+
+    const port = config.port || 8080;
+    
     app.listen(port , ()=> console.log(`App Run With http://localhost:${port}`))
 }
 
---
+++ src/config/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { config } from "./config";
+
+const connectDb = async () => {
+    try {
+        mongoose.connection.on('connected', ()=>{
+            console.log('Connected Successfully!')
+        })
+        mongoose.connection.on('error', (err)=>{
+            console.log('Error in Connecting to Database!', err)
+        })
+        await mongoose.connect(config.databaseUrl as string)
+    } catch (error) {
+        console.log('Failed to Connect Database!', error)
+        process.exit(1)
+    }
+}
+
+export default connectDb
