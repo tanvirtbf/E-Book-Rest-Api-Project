b3ef33ccda95a2f6193cc5b19c122656b09a3f23
tanvirtbf <tanver018765@gmail.com>
2024-10-16 17:18:49 +0600

    advance error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from "express"
 import createHttpError from "http-errors"
 import userModel from "./userModel"
 import bcrypt from 'bcrypt'
+import { sign } from "jsonwebtoken"
+import { config } from "../config/config"
+import { User } from "./userTypes"
 
 const createUser = async(req: Request,res: Response,next: NextFunction)=>{
 
@@ -12,29 +15,52 @@ const createUser = async(req: Request,res: Response,next: NextFunction)=>{
     const error = createHttpError(400, "All Fields are required!")
     return next(error)
   }
-  
-  // Database Call
-  const user = await userModel.findOne({ email : email })
-  
-  if(user){
-    const error = createHttpError(400, "User Already Exists with this email.")
-    return next(error)
-  }
 
+  try {
+
+    // Database Call
+    const user = await userModel.findOne({ email : email })
+    
+    if(user){
+      const error = createHttpError(400, "User Already Exists with this email.")
+      return next(error)
+    }
+    
+  } catch (error) {
+    return next(createHttpError(500, 'Error while getting user!'))
+  }
+  
+
+  
   // password hashed
   const hashedPassword = await bcrypt.hash(password,10)
 
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  })
+  let newUser: User
+  
+  try {
 
-  // Token Generation JWT
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    })
+    
+  } catch (error) {
+    return next(createHttpError(500, 'Error while creating user'))
+  }
+  
+
+  try {
+      // Token Generation JWT
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, { expiresIn: '1d', algorithm: "HS256"})
 
   // response
+  res.json({ accessToken : token })
+  } catch (error) {
+    return next(createHttpError(500,'Error while signing the jwt token!'))
+  }
 
-  res.json({ id : newUser._id })
+
 }
 
 export {createUser}
