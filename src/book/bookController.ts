4025f01ac77be72ec254706fffe6b24953d44d86
tanvirtbf <tanver018765@gmail.com>
2024-10-17 15:22:47 +0600

    files upload

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  const {} = req.body;
-  
+  console.log("files", req.files)
+
   res.json({})
 }
 
