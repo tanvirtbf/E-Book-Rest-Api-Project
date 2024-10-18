36ee8535be781596ae42174031be168db1dde921
tanvirtbf <tanver018765@gmail.com>
2024-10-18 21:48:58 +0600

    token parsed throw header

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,5 +1,11 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const authenticate = (req: Request, res: Response, next: NextFunction) => {
-    
+    const token = req.header('Authorization')
+    if(!token){
+        return next(createHttpError(401, 'Authorization token is required!'))
+    }
+
+    const parsedToken = token.split(' ')[1]
 }
