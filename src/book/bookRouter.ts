135587f7e37da41bf9dbafd06662576990599594
tanvirtbf <tanver018765@gmail.com>
2024-10-18 21:57:00 +0600

    authenticate middleware set

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { createBook } from './bookController'
 import multer from 'multer'
 import path from 'node:path'
+import authenticate from '../middlewares/authenticate'
 
 
 const bookRouter = express.Router()
@@ -15,7 +16,7 @@ const upload = multer({
   limits: { fileSize: 3e7 } // 30mb 
 })
 
-bookRouter.post('/', upload.fields([
+bookRouter.post('/', authenticate , upload.fields([
   { name : 'coverImage', maxCount: 1},
   { name : 'file', maxCount: 1 }
 ]) , createBook)
--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import { verify } from "jsonwebtoken";
+import { config } from "../config/config";
+
 
 const authenticate = (req: Request, res: Response, next: NextFunction) => {
     const token = req.header('Authorization')
@@ -8,4 +11,13 @@ const authenticate = (req: Request, res: Response, next: NextFunction) => {
     }
 
     const parsedToken = token.split(' ')[1]
+
+    // Token Decoded or verified..
+    const decoded = verify(parsedToken, config.jwtSecret as string)
+    console.log('decoded: ', decoded)
+    // req.userId = decoded.sub
+    next()
+
 }
+
+export default authenticate
