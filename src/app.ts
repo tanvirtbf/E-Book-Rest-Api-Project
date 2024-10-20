cca6d7ab9dfd6a5c8b8e5531e8478c1d8321030c
tanvirtbf <tanver018765@gmail.com>
2024-10-20 17:21:42 +0600

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { NextFunction, Request, Response } from "express";
+import cors from 'cors'
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRouter";
+import { config } from "./config/config";
 
 const app = express();
+
+app.use(cors({ origin: config.frontendDomain }))
 
 app.use(express.json()) // jate json body accepted hoy..
 
