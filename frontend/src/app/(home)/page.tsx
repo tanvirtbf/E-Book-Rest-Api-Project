feafc2ff903c08c38a76b53c415cdc59350bf592
tanvirtbf <tanver018765@gmail.com>
2024-10-26 15:19:06 +0600

    product list component create and placing

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/app/(home)/components/Banner";
+import ProductList from "./components/ProductList";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <ProductList />
     </>
   );
 }
