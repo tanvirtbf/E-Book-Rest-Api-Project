4d2379f7c22bcd3fa0b1f95cc12bff4913dcbc72
tanvirtbf <tanver018765@gmail.com>
2024-10-29 23:07:13 +0600

    update

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -13,8 +13,10 @@ const connectDB = async () => {
 
     await mongoose.connect(config.databaseUrl as string);
   } catch (err) {
+
     console.error("Failed to connect to database.", err);
     process.exit(1);
+    
   }
 };
 
