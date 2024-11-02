10ff4d031c650632bd62dd238f4f286f080c907e
tanvirtbf <tanver018765@gmail.com>
2024-11-02 17:21:24 +0600

    Download Button added

--- frontend/src/app/book/[bookId]/page.tsx
+++ frontend/src/app/book/[bookId]/page.tsx
@@ -1,5 +1,6 @@
 import { Book } from "@/types";
 import Image from "next/image";
+import DownloadButton from "../components/DownloadButton";
 
 export default async function SingleBookPage({
   params,
@@ -30,7 +31,7 @@ export default async function SingleBookPage({
         <h2 className="mb-5 text-5xl font-bold leading-[1.1]">{book.title}</h2>
         <span className="font-semibold">by {book.author.name}</span>
         <p className="mt-5 text-lg leading-8">{book.description}</p>
-        {/* <DownloadButton fileLink={book.file} /> */}
+        <DownloadButton fileLink={book.file} />
       </div>
       <div className="flex justify-end">
         <Image
