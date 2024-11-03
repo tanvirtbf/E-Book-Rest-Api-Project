9d35960ca544af765dc4c3928247101f8933226f
tanvirtbf <tanver018765@gmail.com>
2024-11-03 22:59:55 +0600

    dashboard layout complete

--- dashboard/src/router.tsx
+++ dashboard/src/router.tsx
@@ -1,21 +1,33 @@
-import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
+import { createBrowserRouter } from 'react-router-dom';
+import LoginPage from '@/pages/LoginPage';
+import HomePage from '@/pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardLayout from './layouts/DashboardLayout';
+import BooksPage from './pages/BookPage';
 
 const router = createBrowserRouter([
-  {
-    path:'/',
-    element: <HomePage />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '/register',
-    element: <RegisterPage />,
-  },
-])
+    {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+        children: [
+            {
+                path: 'home',
+                element: <HomePage />,
+            },
+            {
+                path: 'books',
+                element: <BooksPage />,
+            },
+        ],
+    },
+    {
+        path: '/login',
+        element: <LoginPage />,
+    },
+    {
+        path: '/register',
+        element: <RegisterPage />,
+    },
+]);
 
-export default router
+export default router;
